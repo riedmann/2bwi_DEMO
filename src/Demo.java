1337def452ba381e4a6865d24547762636801276
anderl <anderl@rocks.com>
2022-02-24 15:55:25 +0100

    initial upload

---
+++ src/Demo.java
@@ -0,0 +1,26 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+        int c = 10;
+        double d = 10.7;
+
+        boolean isHuman = true;
+        String text = "fdsa jfdas fjdsak";
+
+        if (c==4){
+            System.out.println("its 4");
+        } else {
+            System.out.println("else");
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i:" + i);
+
+
+        }
+
+
+    }
+}
